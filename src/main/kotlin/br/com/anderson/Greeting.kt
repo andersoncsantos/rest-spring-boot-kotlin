3f534ed4e4bf21167609db221152fba2fe6b3981
Anderson <andersonc1981@gmail.com>
2023-01-04 17:05:55 -0300

    primeiro endpoint rest

---
+++ src/main/kotlin/br/com/anderson/Greeting.kt
@@ -0,0 +1,4 @@
+package br.com.anderson
+
+class Greeting(val id: Long, val content: String) {
+}
---
+++ src/test/kotlin/br/com/anderson/RestSpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.anderson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestSpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
