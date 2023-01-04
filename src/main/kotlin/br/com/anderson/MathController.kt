c18720e42aad2856014af57672a8298a059a15bd
Anderson <andersonc1981@gmail.com>
2023-01-04 18:56:34 -0300

    definindo as funcoes de validacao

--- src/main/kotlin/br/com/anderson/MathController.kt
+++ src/main/kotlin/br/com/anderson/MathController.kt
@@ -13,16 +13,20 @@ class MathController {
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(@PathVariable(value = "numberOne") numberOne: String?,
             @PathVariable(value = "numberTwo") numberTwo: String?): Double {
-        if (!isNumeric(numberOne) || isNumeric(numberTwo)) throw Exception()
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
     }
 
-    private fun convertToDouble(numberOne: String?): Double {
-        return 0.0
+    private fun convertToDouble(strNumber: String?): Double {
+        if(strNumber.isNullOrBlank()) return 0.0
+        val number = strNumber.replace(",".toRegex(), ".")
+        return if(isNumeric(number)) number.toDouble() else 0.0
     }
 
-    private fun isNumeric(numberOne: String?): Boolean {
-        return false
+    private fun isNumeric(strNumber: String?): Boolean {
+        if (strNumber.isNullOrBlank()) return false
+        val number = strNumber.replace(",".toRegex(), ".")
+        return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
 
 }
