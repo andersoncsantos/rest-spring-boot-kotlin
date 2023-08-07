7fcb0829949407c12feed50208bae1631c621979
Anderson <andersonc1981@gmail.com>
2023-08-07 20:48:05 -0300

    101.1304 content negotiation via HEADER parameter

--- src/main/kotlin/br/com/anderson/config/WebConfig.kt
+++ src/main/kotlin/br/com/anderson/config/WebConfig.kt
@@ -9,12 +9,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+        /*
+
+        passando no  query parameter
+
         configurer.favorParameter(true)
             .parameterName("mediaType")
             .ignoreAcceptHeader(true)
             .useRegisteredExtensionsOnly(false)
             .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_ATOM_XML)
+                .mediaType("xml", MediaType.APPLICATION_ATOM_XML)*/
+
+        /*passando no header */
+        configurer.favorParameter(false)
+            .ignoreAcceptHeader(false)
+            .useRegisteredExtensionsOnly(false)
+            .defaultContentType(MediaType.APPLICATION_JSON)
+            .mediaType("json", MediaType.APPLICATION_JSON)
+            .mediaType("xml", MediaType.APPLICATION_ATOM_XML)
+
+
+
     }
 }
