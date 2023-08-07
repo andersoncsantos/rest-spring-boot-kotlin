dbd7dc2836d739b7035f1b8a72907fea2ee280a9
Anderson <andersonc1981@gmail.com>
2023-08-07 19:25:23 -0300

    96.1202 customizando JSON

--- src/main/kotlin/br/com/anderson/dto/v2/PersonDto.kt
+++ src/main/kotlin/br/com/anderson/dto/v2/PersonDto.kt
@@ -1,13 +1,24 @@
 package br.com.anderson.dto.v2
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import java.time.ZonedDateTime
 
+@JsonPropertyOrder("id", "first_name", "last_name", "gender", "birthDate")
 data class PersonDto(
 
     var id: Long = 0,
+
+    @field:JsonProperty("first_name")
     var firstName: String = "",
+
+    @field:JsonProperty("last_name")
     var lastName: String = "",
+
     var address: String = "",
+
+    @field:JsonIgnore
     var gender: String = "",
     var birthDate: ZonedDateTime? = null
 )
